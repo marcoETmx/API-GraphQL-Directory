667da12e47c35bd0debfc7d8102c8125551f2a4c
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-04-23 06:54:27 -0500

    Mejorando la esctructura del proyecto

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+const { buildSchema } = require('graphql')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.port || 3000
+
+const schema = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8'))
+
+app.use('/api', gqlMiddleware({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
