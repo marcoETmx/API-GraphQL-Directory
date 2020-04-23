1bb384a073df4c29fbca9454a88670d849a36648
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-04-23 11:19:00 -0500

    Se agrega la creación de estudiantes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
-const { buildSchema } = require('graphql')
+require('dotenv').config();
+const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
@@ -8,8 +9,8 @@ const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
-
-const schema = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8'))
+const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8');
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 app.use('/api', gqlMiddleware({
   schema: schema,
