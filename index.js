45411073c8329fcd7fd09ce0fc5293b678221b8f
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-04-23 20:01:24 -0500

    Preparando para produccion

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
+const cors = require('cors')
+const idDEV =  process.env.NODE_ENV !== 'production'
 
 const app = express()
 const port = process.env.port || 3000
 const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8');
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
+app.use(cors());
 app.use('/api', gqlMiddleware({
   schema: schema,
   rootValue: resolvers,
-  graphiql: true
+  graphiql: idDEV
 }))
 
 app.listen(port, () => {
